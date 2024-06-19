39c2b6c578d46ff5c50a2efd9141897751b01adc
LoefarsOsu <loefarsosu@gmail.com>
2024-06-19 02:55:02 -0400

    Alignment is now enum-based for language purposes. Adjusted lang files accordingly.

--- src/main/java/com/loefars/igttracker/PlaytimeConfig.java
+++ src/main/java/com/loefars/igttracker/PlaytimeConfig.java
@@ -4,6 +4,7 @@ import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.ui.core.Color;
 
+
 @Modmenu(modId = "igttracker")
 @Config(name = "igttracker-config", wrapperName = "IGTTrackerConfig")
 public class PlaytimeConfig {
@@ -12,7 +13,9 @@ public class PlaytimeConfig {
     public float scale = 1;
     public int xPos = 24;
     public int yPos = 24;
-    public boolean align = false;
-
-
+    public enum Alignment {
+        LEFT,
+        RIGHT;
+    }
+    public Alignment align = Alignment.LEFT;
 }
